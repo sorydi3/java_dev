60c23d37e433362ebb633fa9e7c9a37f0de8d955
ibradi3 <ibrahimasory22@hotmail.com>
2022-10-03 21:38:24 +0200

    delete unused imports

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/NotePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/NotePage.java
@@ -1,6 +1,5 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
